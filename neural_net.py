4493ae98b293208d1c73d2ae96484a7f938e90a0
Jocelyn Chang <jrchang@engineer.com>
2014-07-12 12:38:08 -0700

    rev1 -loop not quite right

--- neural_net.py
+++ neural_net.py
@@ -4,6 +4,7 @@ import math
 class Neuron(object):
 
     def __init__(self, inputs, weights):
+ 
         self.inputs = inputs
         self.weights = weights
 
@@ -14,19 +15,61 @@ class Neuron(object):
 
         for i in range(0, len(self.inputs)):
             z=z+self.inputs[i]*self.weights[i]
-        print z
-        return
+        print "z=", z
+        return z
     
     def calc_sigmoid(self, z):
+        """
+        o = s(z) = 1.0 / (1.0 + e**(-z)) 
+        ds(o)/dz = s(z) * (1 - s(z)) = o * (1 - o)
+        """
         s=1/(1+math.exp(-z))
         print s
-        return
+        return s
 
-def RunNetwork():
+    def calc_dw(self, index, perf_elem, z):
+        w1=(perf_elem.d-z)*self.inputs[index]-self.weights[index]
+        #update weights
+        self.weights[index]=w1
+        return 
 
-    n=Neuron([1, 2, 3], [0.1, 0.1, 0.9])
+class PerformanceElement(object):
+    
+    def __init__(self, desired):
+        self.d=desired
 
-    z=n.calc_z()
+    def calc_perf(self, o):
+        """
+        P(o) = -0.5 (d - o)**2 
+        dP(o)/dx = (d - o) 
+        """
+        print "d-0=", self.d-o
+        return self.d-o
+    
+def run_node():
 
-    n.calc_sigmoid(z)
+    input_list=[1, 2, 3]
+    output_list=[0.1, 0.1, 0.9]
+    desired_out=0.0
+    
+    n=Neuron(input_list, output_list)
+    p=PerformanceElement(desired_out)
+
+    i=0
+    
+    for i in range(0, 10):
+        #calculate sum of inputs into node
+        z=n.calc_z()
+
+        #calculate performance criteria
+        #p.calc_perf(o)
+
+        #calculate sigmoid
+        #o= n.calc_sigmoid(z)        
+        
+        #recalulate weights
+        for i_list in range(0, len(input_list)):
+            n.calc_dw(i_list, p, z)
+            
+
     return
